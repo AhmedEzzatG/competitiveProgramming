30fad8e17fd146523342f52d3c2878798e4d41f3
AhmedEzzatG <ahmed.ezzat.ga@gmail.com>
2019-09-19 21:32:40 +0200

    DP

---
+++ DPproblems/11-EggDropping.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+#define endl "\n"
+#define oo 0x3f3f3f3fLL
+#define OO (oo*oo)
+#define sz(s) (int)(s.size())
+#define RT(s) return cout<<s,0
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define clr(v,val) memset(v,val,sizeof(v))
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const ld PI = acos(-1), EPS = 1e-8;
+const int mod = 1000000007;
+const int dr[]{ -1, -1, 0, 1, 1, 1, 0, -1 };
+const int dc[]{ 0, 1, 1, 1, 0, -1, -1, -1 };
+void run() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.in", "r", stdin);
+	freopen("output.out", "w", stdout);
+#else
+	//freopen("input.in", "r", stdin);
+#endif
+}
+
+int mem[11][51];
+int eggDropping(int eggs, int floors) {
+	if (eggs == 1 || floors < 1)return floors;
+	int& rt = mem[eggs][floors];
+	if (~rt)return rt;
+	rt = oo;
+	for (int i = 1; i <= floors; i++)
+		rt = min(rt, 1 +
+			max(eggDropping(eggs - 1, i - 1),
+				eggDropping(eggs, floors - i)));
+	return rt;
+}
+
+int main() {
+	run();
+	clr(mem, -1);
+	int t; cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		cout << eggDropping(n, k) << endl;
+	}
+}
---
+++ DPproblems/18-WeightedJobSchedulingMaximumProfit.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define endl "\n"
+#define oo 0x3f3f3f3fLL
+#define OO (oo*oo)
+#define sz(s) (int)(s.size())
+#define RT(s) return cout<<s,0
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define clr(v,val) memset(v,val,sizeof(v))
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const ld PI = acos(-1), EPS = 1e-8;
+const int mod = 1000000007;
+const int dr[]{ -1, -1, 0, 1, 1, 1, 0, -1 };
+const int dc[]{ 0, 1, 1, 1, 0, -1, -1, -1 };
+void run() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.in", "r", stdin);
+	//freopen("output.out", "w", stdout);
+#else
+	//freopen("input.in", "r", stdin);
+#endif
+}
+
+struct job {
+	int start, end, profit;
+	bool operator <(const job& other)const {
+		return end < other.end;
+	}
+};
+const int MAX = 1001;
+int mem[MAX][MAX];
+vector<job> v;
+int  maximumProfit(int index, int last) {
+	if (index == sz(v))return 0;
+	int& rt = mem[index][last];
+	if (~rt)return rt;
+	rt = maximumProfit(index + 1, last);
+	if (last == sz(v) || v[last].end <= v[index].start)
+		rt = max(rt, v[index].profit + maximumProfit(index + 1, index));
+	return rt;
+}
+
+int main() {
+	run();
+	clr(mem, -1);
+	int n; cin >> n;
+	v.resize(n);
+	for (auto& a : v)cin >> a.start >> a.end >> a.profit;
+	sort(all(v));
+	cout << maximumProfit(0, sz(v));
+}
